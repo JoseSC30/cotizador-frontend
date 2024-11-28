0f0b75e644e9d53c081ea3624d7967334b751fbd
Juana Barriga <juanitapadilla483@gmail.com>
2024-11-27 22:48:23 -0400

    vista home,chat,responsiv

--- src/app/customer/layouts/customer-layout/customer-layout.component.ts
+++ src/app/customer/layouts/customer-layout/customer-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { MenuItem } from 'src/app/shared/interfaces/menu-item.interface';
 
@@ -8,25 +8,44 @@ import { MenuItem } from 'src/app/shared/interfaces/menu-item.interface';
   styleUrls: ['./customer-layout.component.css'],
 })
 export class CustomerLayoutComponent {
-  showSidebar: boolean = true;
-  showFullNavbar: boolean = false;
+  showSidebar: boolean = true; // Menú lateral visible por defecto
+  showFullNavbar: boolean = false; // Navbar expandido si el menú lateral está oculto
+  isMobile: boolean = false; // Detecta si es una vista móvil
   menuItems!: MenuItem[];
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     this.menuItems = [
       { icon: 'bx-home', name: 'Inicio', route: '/dashboard/home' },
-      { icon: 'bx-chat', name: 'Mensajes', route: '/dashboard/chats' },
+      { icon: 'bx-chat', name: 'Cotizar', route: '/dashboard/chats' },
       { icon: 'bx-cog', name: 'Configuración', route: '/dashboard/settings' },
     ];
 
+    // Detectar cambios de ruta para ocultar o mostrar el menú lateral
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.showSidebar =
-          !this.activatedRoute.firstChild?.snapshot.routeConfig?.path?.includes(
-            'chats'
-          );
-        this.showFullNavbar = !this.showSidebar;
+        const isChatsRoute = event.url.includes('/dashboard/chats');
+        this.showSidebar = !isChatsRoute;
+        this.showFullNavbar = isChatsRoute;
       }
     });
+
+    this.checkScreenSize(); // Llamada inicial
+  }
+
+  // Detectar cambios en el tamaño de la pantalla
+  @HostListener('window:resize', [])
+  checkScreenSize(): void {
+    this.isMobile = window.innerWidth <= 768; // Detecta si es móvil
+    if (!this.isMobile) {
+      this.showSidebar = true; // Siempre mostrar el menú en pantallas grandes
+    }
+  }
+
+  // Alterna la visibilidad del menú lateral solo en móviles
+  toggleSidebar(): void {
+    if (this.isMobile) {
+      this.showSidebar = !this.showSidebar;
+    }
   }
 }
+
